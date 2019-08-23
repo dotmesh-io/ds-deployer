13c129e1d936846c925b66ad64dd6a661a57aace
Karolis Rusenas <karolis.rusenas@gmail.com>
2019-08-23 15:33:15 +0100

    beginnigns of controller

---
+++ internal/controller/cache.go
@@ -0,0 +1,23 @@
+package controller
+
+import (
+	appsv1 "k8s.io/api/apps/v1"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/api/extensions/v1beta1"
+
+	deployer_v1 "github.com/dotmesh-io/ds-deployer/apis/deployer/v1"
+)
+
+// A KubernetesCache holds Kubernetes objects and associated configuration and produces
+// DAG values.
+type KubernetesCache struct {
+	ingresses        map[Meta]*v1beta1.Ingress
+	deployments      map[Meta]*appsv1.Deployment
+	services         map[Meta]*v1.Service
+	modelDeployments map[Meta]*deployer_v1.Deployment
+}
+
+// Meta holds the name and namespace of a Kubernetes object.
+type Meta struct {
+	name, namespace string
+}
--- internal/controller/deployment_controller.go
+++ internal/controller/deployment_controller.go
@@ -1,1 +1,61 @@
 package controller
+
+import (
+	"context"
+
+	"github.com/go-logr/logr"
+
+	appsv1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+)
+
+// reconcileDeployment reconciles Deployments
+type reconcileDeployment struct {
+	// client can be used to retrieve objects from the APIServer.
+	client client.Client
+	log    logr.Logger
+}
+
+// Implement reconcile.Reconciler so the controller can reconcile objects
+var _ reconcile.Reconciler = &reconcileDeployment{}
+
+func (r *reconcileDeployment) Reconcile(request reconcile.Request) (reconcile.Result, error) {
+	// set up a convenient log object so we don't have to type request over and over again
+	log := r.log.WithValues("request", request)
+
+	// Fetch the Deployment from the cache
+	deployment := &appsv1.Deployment{}
+	err := r.client.Get(context.TODO(), request.NamespacedName, deployment)
+	if errors.IsNotFound(err) {
+		log.Error(nil, "Could not find Deployment")
+		return reconcile.Result{}, nil
+	}
+
+	if err != nil {
+		log.Error(err, "Could not fetch Deployment")
+		return reconcile.Result{}, err
+	}
+
+	// Print the Deployment
+	log.Info("Reconciling Deployment", "container name", deployment.Spec.Template.Spec.Containers[0].Name)
+
+	// Set the label if it is missing
+	if deployment.Labels == nil {
+		deployment.Labels = map[string]string{}
+	}
+	if deployment.Labels["hello"] == "world" {
+		return reconcile.Result{}, nil
+	}
+
+	// Update the Deployment
+	deployment.Labels["hello"] = "world"
+	err = r.client.Update(context.TODO(), deployment)
+	if err != nil {
+		log.Error(err, "Could not write Deployment")
+		return reconcile.Result{}, err
+	}
+
+	return reconcile.Result{}, nil
+}
