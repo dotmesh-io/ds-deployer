53ed52a571ec6b5546e57de33419f8759dbe5938
Karolis Rusenas <karolis.rusenas@gmail.com>
2019-08-28 14:19:36 +0100

    controller init

--- cmd/ds-deployer/main.go
+++ cmd/ds-deployer/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"context"
 	"os"
+
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
 
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
@@ -15,85 +18,144 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/source"
 
 	deploymentController "github.com/dotmesh-io/ds-deployer/internal/controller"
+	"github.com/dotmesh-io/ds-deployer/pkg/deployer"
 	"github.com/dotmesh-io/ds-deployer/pkg/logger"
+	"github.com/dotmesh-io/ds-deployer/pkg/workgroup"
 )
 
+const EnvGatewayAddress = "GATEWAY_ADDRESS"
+const EnvAuthToken = "TOKEN"
+
+const gatewayServerAddress = "cloud.dotscience.com:8800"
 const controllerName = "deployment-controller"
 
 func main() {
 
+	app := kingpin.New("ds-deployer", "DotScience runner")
+
+	run := app.Command("run", "Start the deployer")
+	token := run.Flag("token", "Authentication token (each registered runner gets a token)").Default(os.Getenv(EnvAuthToken)).String()
+	requireTLS := run.Flag("require-tls", "Require TLS for connection to the server").Default("true").Bool()
+	serverAddr := run.Flag("addr", "Server address").Default(gatewayServerAddress).String()
+
 	logger := logger.GetInstance().Sugar()
 
-	// Setup a Manager
-	logger.Info("setting up manager")
-	mgr, err := manager.New(config.GetConfigOrDie(), manager.Options{})
-	if err != nil {
-		logger.Errorw("unable to set up overall controller manager",
-			"error", err,
-		)
-		os.Exit(1)
+	args := os.Args[1:]
+	switch kingpin.MustParse(app.Parse(args)) {
+	default:
+		app.Usage(args)
+		os.Exit(2)
+	case run.FullCommand():
+		// Setup a Manager
+		logger.Info("setting up manager")
+		mgr, err := manager.New(config.GetConfigOrDie(), manager.Options{})
+		if err != nil {
+			logger.Errorw("unable to set up overall controller manager",
+				"error", err,
+			)
+			os.Exit(1)
+		}
+
+		cache := deploymentController.NewKubernetesCache()
+
+		controllerOptions := []deploymentController.Option{
+			deploymentController.WithClient(mgr.GetClient()),
+			deploymentController.WithCache(cache),
+			deploymentController.WithLogger(logger.With("module", "deployment-reconciler")),
+		}
+
+		deploymentReconciler, err := deploymentController.New(controllerOptions...)
+		if err != nil {
+			logger.Errorw("unable to set up dotscience deployment controller",
+				"error", err,
+			)
+			os.Exit(1)
+		}
+
+		// Setup a new controller to reconcile dotscience deployments
+		logger.Info("Setting up controller")
+		c, err := controller.New(controllerName, mgr, controller.Options{
+			Reconciler: deploymentReconciler,
+		})
+		if err != nil {
+			logger.Errorw("unable to set up individual controller",
+				"error", err,
+			)
+			os.Exit(1)
+		}
+
+		// Watch Deployment and enqueue ReplicaSet object key
+		if err := c.Watch(&source.Kind{Type: &appsv1.Deployment{}}, &handler.EnqueueRequestForObject{}); err != nil {
+			logger.Error("unable to watch Deployment",
+				"error", err,
+			)
+			os.Exit(1)
+		}
+
+		if err := c.Watch(&source.Kind{Type: &v1beta1.Ingress{}}, &handler.EnqueueRequestForObject{}); err != nil {
+			logger.Error("unable to watch Ingress",
+				"error", err,
+			)
+			os.Exit(1)
+		}
+
+		if err := c.Watch(&source.Kind{Type: &corev1.Service{}}, &handler.EnqueueRequestForObject{}); err != nil {
+
+			logger.Error("unable to watch Service",
+				"error", err,
+			)
+			os.Exit(1)
+		}
+
+		// // Watch Pods and enqueue owning ReplicaSet key
+		// if err := c.Watch(&source.Kind{Type: &corev1.Pod{}},
+		// 	&handler.EnqueueRequestForOwner{OwnerType: &appsv1.ReplicaSet{}, IsController: true}); err != nil {
+		// 	entryLog.Error(err, "unable to watch Pods")
+		// 	os.Exit(1)
+		// }
+
+		gatewayAddress := *serverAddr
+		if os.Getenv(EnvGatewayAddress) != "" {
+			gatewayAddress = os.Getenv(EnvGatewayAddress)
+		}
+
+		gatewayClient := deployer.New(&deployer.Opts{
+			Addr:        gatewayAddress,
+			Token:       *token,
+			RequireTLS:  *requireTLS,
+			ObjectCache: cache,
+		})
+
+		var g workgroup.Group
+
+		g.Add(func(stop <-chan struct{}) error {
+			logger.Info("starting manager")
+			if err := mgr.Start(signals.SetupSignalHandler()); err != nil {
+				logger.Error("unable to run manager",
+					"error", err,
+				)
+				return err
+			}
+
+			return nil
+		})
+
+		g.Add(func(stop <-chan struct{}) error {
+			ctx, cancel := context.WithCancel(context.Background())
+
+			go func() {
+				<-stop
+				cancel()
+			}()
+
+			return gatewayClient.StartDeployer(ctx)
+		})
+
+		err = g.Run()
+		if err != nil {
+			logger.Errorf("deployer stopped with an error: %s", err)
+			os.Exit(1)
+		}
 	}
 
-	controllerOptions := []deploymentController.Option{
-		deploymentController.WithClient(mgr.GetClient()),
-		deploymentController.WithLogger(logger.With("module", "deployment-reconciler")),
-	}
-
-	deploymentReconciler, err := deploymentController.New(controllerOptions...)
-	if err != nil {
-		logger.Errorw("unable to set up dotscience deployment controller",
-			"error", err,
-		)
-		os.Exit(1)
-	}
-
-	// Setup a new controller to reconcile dotscience deployments
-	logger.Info("Setting up controller")
-	c, err := controller.New(controllerName, mgr, controller.Options{
-		Reconciler: deploymentReconciler,
-	})
-	if err != nil {
-		logger.Errorw("unable to set up individual controller",
-			"error", err,
-		)
-		os.Exit(1)
-	}
-
-	// Watch Deployment and enqueue ReplicaSet object key
-	if err := c.Watch(&source.Kind{Type: &appsv1.Deployment{}}, &handler.EnqueueRequestForObject{}); err != nil {
-		logger.Error("unable to watch Deployment",
-			"error", err,
-		)
-		os.Exit(1)
-	}
-
-	if err := c.Watch(&source.Kind{Type: &v1beta1.Ingress{}}, &handler.EnqueueRequestForObject{}); err != nil {
-		logger.Error("unable to watch Ingress",
-			"error", err,
-		)
-		os.Exit(1)
-	}
-
-	if err := c.Watch(&source.Kind{Type: &corev1.Service{}}, &handler.EnqueueRequestForObject{}); err != nil {
-
-		logger.Error("unable to watch Service",
-			"error", err,
-		)
-		os.Exit(1)
-	}
-
-	// // Watch Pods and enqueue owning ReplicaSet key
-	// if err := c.Watch(&source.Kind{Type: &corev1.Pod{}},
-	// 	&handler.EnqueueRequestForOwner{OwnerType: &appsv1.ReplicaSet{}, IsController: true}); err != nil {
-	// 	entryLog.Error(err, "unable to watch Pods")
-	// 	os.Exit(1)
-	// }
-
-	logger.Info("starting manager")
-	if err := mgr.Start(signals.SetupSignalHandler()); err != nil {
-		logger.Error("unable to run manager",
-			"error", err,
-		)
-		os.Exit(1)
-	}
 }
