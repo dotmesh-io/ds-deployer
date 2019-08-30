053efd62153d8e94981421e38d65c888dac6c75a
Karolis Rusenas <karolis.rusenas@gmail.com>
2019-08-30 11:44:51 +0100

    cache k8s handlers

--- internal/controller/cache.go
+++ internal/controller/cache.go
@@ -4,8 +4,11 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/api/extensions/v1beta1"
+	"k8s.io/client-go/tools/cache"
 
 	deployer_v1 "github.com/dotmesh-io/ds-deployer/apis/deployer/v1"
+
+	"github.com/dotmesh-io/ds-deployer/pkg/cond"
 )
 
 const DEFAULT_INGRESS_CLASS = "ds-deployer"
@@ -22,6 +25,8 @@ type KubernetesCache struct {
 	deployments      map[Meta]*appsv1.Deployment
 	services         map[Meta]*corev1.Service
 	modelDeployments map[Meta]*deployer_v1.Deployment
+
+	cond.Cond
 }
 
 func NewKubernetesCache() *KubernetesCache {
@@ -33,11 +38,26 @@ type Meta struct {
 	name, namespace string
 }
 
+func (kc *KubernetesCache) OnAdd(obj interface{}) {
+	_ = kc.Insert(obj)
+}
+
+func (kc *KubernetesCache) OnUpdate(_, newObj interface{}) {
+	_ = kc.Insert(newObj)
+}
+
+func (kc *KubernetesCache) OnDelete(obj interface{}) {
+	_ = kc.Remove(obj)
+}
+
 // Insert inserts obj into the KubernetesCache.
 // Insert returns true if the cache accepted the object, or false if the value
 // is not interesting to the cache. If an object with a matching type, name,
 // and namespace exists, it will be overwritten.
 func (kc *KubernetesCache) Insert(obj interface{}) bool {
+	// notify subscribers about cache changes
+	defer kc.Notify()
+
 	switch obj := obj.(type) {
 	case *corev1.Service:
 		m := Meta{name: obj.Name, namespace: obj.Namespace}
@@ -52,6 +72,7 @@ func (kc *KubernetesCache) Insert(obj interface{}) bool {
 		if class != "" && class != kc.ingressClass() {
 			return false
 		}
+
 		m := Meta{name: obj.Name, namespace: obj.Namespace}
 		if kc.ingresses == nil {
 			kc.ingresses = make(map[Meta]*v1beta1.Ingress)
@@ -76,6 +97,56 @@ func (kc *KubernetesCache) Insert(obj interface{}) bool {
 		// not an interesting object
 		return false
 	}
+}
+
+// Remove removes obj from the KubernetesCache.
+// Remove returns a boolean indiciating if the cache changed after the remove operation.
+func (kc *KubernetesCache) Remove(obj interface{}) bool {
+	switch obj := obj.(type) {
+	default:
+		return kc.remove(obj)
+	case cache.DeletedFinalStateUnknown:
+		return kc.Remove(obj.Obj) // recurse into ourselves with the tombstoned value
+	}
+}
+
+func (kc *KubernetesCache) remove(obj interface{}) bool {
+	switch obj := obj.(type) {
+	case *corev1.Service:
+		m := Meta{name: obj.Name, namespace: obj.Namespace}
+		_, ok := kc.services[m]
+		delete(kc.services, m)
+		return ok
+	case *v1beta1.Ingress:
+		m := Meta{name: obj.Name, namespace: obj.Namespace}
+		_, ok := kc.ingresses[m]
+		delete(kc.ingresses, m)
+		return ok
+	case *appsv1.Deployment:
+		m := Meta{name: obj.Name, namespace: obj.Namespace}
+		_, ok := kc.deployments[m]
+		delete(kc.deployments, m)
+		return ok
+	case *deployer_v1.Deployment:
+		m := Meta{name: obj.Name, namespace: obj.Namespace}
+		_, ok := kc.modelDeployments[m]
+		delete(kc.modelDeployments, m)
+		return ok
+	default:
+		// not interesting
+		return false
+	}
+}
+
+// ModelDeployments returns model deployments
+func (kc *KubernetesCache) ModelDeployments() []*deployer_v1.Deployment {
+	var deployments []*deployer_v1.Deployment
+
+	for _, v := range kc.modelDeployments {
+		deployments = append(deployments, v)
+	}
+
+	return deployments
 }
 
 // ingressClass returns the IngressClass
