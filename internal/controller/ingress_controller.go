e9f304c682e270e4efb0bc3e301b5edfa26f7aad
Karolis Rusenas <karolis.rusenas@gmail.com>
2019-09-04 16:27:13 +0100

    ingress plumbing

--- internal/controller/ingress_controller.go
+++ internal/controller/ingress_controller.go
@@ -1,2 +1,111 @@
 package controller
 
+import (
+	"context"
+
+	deployer_v1 "github.com/dotmesh-io/ds-deployer/apis/deployer/v1"
+	"k8s.io/api/extensions/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+)
+
+func (c *Controller) synchronizeIngresses() error {
+	return nil
+}
+
+func (c *Controller) createIngress(modelDeployment *deployer_v1.Deployment) error {
+	return c.client.Create(context.Background(), toKubernetesIngress(modelDeployment, c.controllerIdentifier))
+}
+
+func getIngressSpec(md *deployer_v1.Deployment) v1beta1.IngressSpec {
+	var modelPort intstr.IntOrString
+
+	for _, p := range md.Service.GetPorts() {
+		if p.GetName() == "model-http" {
+			modelPort = intstr.IntOrString{
+				Type:   intstr.Int,
+				IntVal: p.GetPort(),
+			}
+		}
+	}
+	return v1beta1.IngressSpec{
+		Rules: []v1beta1.IngressRule{
+			{
+				Host: md.Ingress.GetHost(),
+				IngressRuleValue: v1beta1.IngressRuleValue{
+					HTTP: &v1beta1.HTTPIngressRuleValue{
+						Paths: []v1beta1.HTTPIngressPath{
+							{
+								Path: "/",
+								Backend: v1beta1.IngressBackend{
+									ServiceName: getDeploymentName(md),
+									ServicePort: modelPort,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func ingressesEqual(desired, existing *v1beta1.Ingress) bool {
+	if len(desired.Spec.Rules) != len(existing.Spec.Rules) {
+		return false
+	}
+
+	for i := range desired.Spec.Rules {
+		if desired.Spec.Rules[i].Host != existing.Spec.Rules[i].Host {
+			return false
+		}
+		if len(desired.Spec.Rules[i].HTTP.Paths) != len(existing.Spec.Rules[i].HTTP.Paths) {
+			return false
+		}
+
+		for idx := range desired.Spec.Rules[i].HTTP.Paths {
+			if desired.Spec.Rules[i].HTTP.Paths[idx].Path != existing.Spec.Rules[i].HTTP.Paths[idx].Path {
+				return false
+			}
+			if desired.Spec.Rules[i].HTTP.Paths[idx].Backend.ServiceName != existing.Spec.Rules[i].HTTP.Paths[idx].Backend.ServiceName {
+				return false
+			}
+			if desired.Spec.Rules[i].HTTP.Paths[idx].Backend.ServicePort != existing.Spec.Rules[i].HTTP.Paths[idx].Backend.ServicePort {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+func updateIngress(existing v1beta1.Ingress, md *deployer_v1.Deployment) *v1beta1.Ingress {
+	updated := existing.DeepCopy()
+
+	updated.Spec = getIngressSpec(md)
+
+	return updated
+}
+
+func toKubernetesIngress(md *deployer_v1.Deployment, controllerIdentifier string) *v1beta1.Ingress {
+
+	ingress := &v1beta1.Ingress{
+		TypeMeta: metav1.TypeMeta{},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      getDeploymentName(md),
+			Namespace: md.GetNamespace(),
+			Labels: map[string]string{
+				"owner": "ds-deployer",
+			},
+			Annotations: map[string]string{
+				AnnControllerIdentifier: controllerIdentifier,
+				// based on model deployment name we will need this later
+				// to ensure we delete what's not needed anymore
+				"name": md.GetName(),
+			},
+		},
+		Spec: getIngressSpec(md),
+	}
+
+	return ingress
+}
