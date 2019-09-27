d3006edc6927fa7da21777dca75d184f23d03872
Karolis Rusenas <karolis.rusenas@gmail.com>
2019-09-27 16:34:52 +0100

    replicas

--- pkg/status/status.go
+++ pkg/status/status.go
@@ -1,10 +1,14 @@
 package status
 
 import (
+	"fmt"
 	"sync"
 
 	"github.com/dotmesh-io/ds-deployer/pkg/cond"
 )
+
+// static check
+var _ Cache = &StatusCache{}
 
 type StatusCache struct {
 	deployments map[string]DeploymentStatus
@@ -14,8 +18,10 @@ type StatusCache struct {
 
 type Cache interface {
 	Set(deploymentID string, module Module, status Status)
+	SetAvailableReplicas(deploymentID string, replicas int64)
 	Get(deploymentID string) DeploymentStatus
 	Delete(deploymentID string)
+	List() map[string]DeploymentStatus
 }
 
 func New() *StatusCache {
@@ -34,10 +40,30 @@ const (
 	StatusError
 )
 
+func (s Status) String() string {
+	switch s {
+	case StatusNone:
+		return "none"
+	case StatusConfiguring:
+		return "configuring"
+	case StatusReady:
+		return "ready"
+	case StatusError:
+		return "error"
+	}
+	return "unknown"
+}
+
 type DeploymentStatus struct {
-	Deployment Status
-	Service    Status
-	Ingress    Status
+	Deployment        Status
+	AvailableReplicas int64
+	Service           Status
+	Ingress           Status
+}
+
+// Status returns overall status of the deployment
+func (s *DeploymentStatus) Status() string {
+	return fmt.Sprintf("Deployment: %s Service: %s Ingress: %s", s.Deployment, s.Service, s.Ingress)
 }
 
 type Module int
@@ -70,6 +96,21 @@ func (c *StatusCache) Set(deploymentID string, module Module, status Status) {
 	c.mu.Unlock()
 }
 
+func (c *StatusCache) SetAvailableReplicas(deploymentID string, replicas int64) {
+	defer c.Notify()
+	c.mu.Lock()
+	deploymentStatus, ok := c.deployments[deploymentID]
+	if !ok {
+		deploymentStatus = DeploymentStatus{}
+	}
+
+	deploymentStatus.AvailableReplicas = replicas
+
+	c.deployments[deploymentID] = deploymentStatus
+
+	c.mu.Unlock()
+}
+
 func (c *StatusCache) Get(deploymentID string) DeploymentStatus {
 	c.mu.RLock()
 	defer c.mu.RUnlock()
@@ -88,4 +129,19 @@ func (c *StatusCache) Delete(deploymentID string) {
 	c.mu.Lock()
 	delete(c.deployments, deploymentID)
 	c.mu.Unlock()
+}
+
+func (c *StatusCache) List() map[string]DeploymentStatus {
+	result := make(map[string]DeploymentStatus)
+	c.mu.RLock()
+
+	for k, v := range c.deployments {
+		ds := new(DeploymentStatus)
+		*ds = v
+
+		result[k] = *ds
+	}
+
+	c.mu.RUnlock()
+	return result
 }
