8b858578c5323aad00ea738128f2cb20e1fc7a6b
Karolis Rusenas <karolis.rusenas@gmail.com>
2019-10-24 18:02:13 +0100

    test for ingress and dep

--- internal/controller/cache_test.go
+++ internal/controller/cache_test.go
@@ -4,7 +4,9 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/api/extensions/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	deployer_v1 "github.com/dotmesh-io/ds-deployer/apis/deployer/v1"
@@ -75,4 +77,52 @@ func TestGetCachedService(t *testing.T) {
 	svc.Namespace = "new-namespace"
 
 	assert.Equal(t, "foo", c.services[Meta{namespace: "foo", name: "bar"}].Namespace)
+}
+
+func TestGetCachedDeployment(t *testing.T) {
+	c := NewKubernetesCache("123", zap.S())
+
+	inserted := c.Insert(&appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "foo",
+			Name:      "bar",
+			Annotations: map[string]string{
+				AnnControllerIdentifier: "123",
+			},
+		},
+	})
+	assert.True(t, inserted)
+
+	c.Insert(inserted)
+
+	dep, ok := c.GetDeployment("foo", "bar")
+	assert.Equal(t, ok, true)
+
+	dep.Namespace = "new-namespace"
+
+	assert.Equal(t, "foo", c.deployments[Meta{namespace: "foo", name: "bar"}].Namespace)
+}
+
+func TestGetCachedIngress(t *testing.T) {
+	c := NewKubernetesCache("123", zap.S())
+
+	inserted := c.Insert(&v1beta1.Ingress{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "foo",
+			Name:      "bar",
+			Annotations: map[string]string{
+				AnnControllerIdentifier: "123",
+			},
+		},
+	})
+	assert.True(t, inserted)
+
+	c.Insert(inserted)
+
+	ing, ok := c.GetIngress("foo", "bar")
+	assert.Equal(t, ok, true)
+
+	ing.Namespace = "new-namespace"
+
+	assert.Equal(t, "foo", c.ingresses[Meta{namespace: "foo", name: "bar"}].Namespace)
 }
