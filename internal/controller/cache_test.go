45de17e1c1d18e83736603790cbdea6fbf4bcc1e
Karolis Rusenas <karolis.rusenas@gmail.com>
2019-10-24 18:00:09 +0100

    test for cache

--- internal/controller/cache_test.go
+++ internal/controller/cache_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	deployer_v1 "github.com/dotmesh-io/ds-deployer/apis/deployer/v1"
 	"go.uber.org/zap"
@@ -48,4 +50,29 @@ func TestGetCachedDeployments(t *testing.T) {
 	c.Insert(dep)
 
 	assert.Equal(t, "original", cached[0].Namespace)
+}
+
+//  test to ensure that we get copies of objcets
+func TestGetCachedService(t *testing.T) {
+	c := NewKubernetesCache("123", zap.S())
+
+	inserted := c.Insert(&corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "foo",
+			Name:      "bar",
+			Annotations: map[string]string{
+				AnnControllerIdentifier: "123",
+			},
+		},
+	})
+	assert.True(t, inserted)
+
+	c.Insert(inserted)
+
+	svc, ok := c.GetService("foo", "bar")
+	assert.Equal(t, ok, true)
+
+	svc.Namespace = "new-namespace"
+
+	assert.Equal(t, "foo", c.services[Meta{namespace: "foo", name: "bar"}].Namespace)
 }
