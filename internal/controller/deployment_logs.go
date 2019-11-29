58a54c3b74179734a15dd953a6fe9839e767757d
Karolis Rusenas <karolis.rusenas@gmail.com>
2019-11-29 11:46:53 +0000

    getting container names

--- internal/controller/deployment_logs.go
+++ internal/controller/deployment_logs.go
@@ -3,10 +3,13 @@ package controller
 import (
 	"errors"
 	"io"
-
-	deployer_v1 "github.com/dotmesh-io/ds-deployer/apis/deployer/v1"
+	"strings"
 
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+
+	deployer_v1 "github.com/dotmesh-io/ds-deployer/apis/deployer/v1"
 )
 
 var (
@@ -24,14 +27,51 @@ func (c *Controller) Logs(request *deployer_v1.LogsRequest) (io.ReadCloser, erro
 
 	var podName string
 
-	switch request.Container {
-	case deployer_v1.LogsRequest_MODEL:
-		podName = getPodName(md)
-	case deployer_v1.LogsRequest_PROXY:
-		podName = getModelProxyPodName(md)
+	set := labels.Set(map[string]string{
+		"deployment": md.GetId(),
+	})
+
+	if pods, err := c.clientSet.CoreV1().Pods("").List(metav1.ListOptions{
+		LabelSelector: set.AsSelector().String(),
+	}); err != nil {
+		c.logger.Errorf("list pods of deployment %s error:%v", md.Id, err)
+		return nil, err
+	} else {
+
+		if len(pods.Items) == 1 {
+			podName = pods.Items[0].GetName()
+
+			if len(pods.Items[0].Spec.Containers) > 1 {
+
+				switch request.Container {
+				case deployer_v1.LogsRequest_MODEL:
+					podLogOpts.Container = getModelContainerName(pods.Items[0].Spec.Containers)
+				case deployer_v1.LogsRequest_PROXY:
+					podLogOpts.Container = getModelProxyContainerName(pods.Items[0].Spec.Containers)
+				}
+			}
+		}
 	}
 
 	req := c.clientSet.CoreV1().Pods(md.GetNamespace()).GetLogs(podName, podLogOpts)
 	return req.Stream()
 
+}
+
+func getModelContainerName(containers []corev1.Container) string {
+	for _, c := range containers {
+		if strings.HasPrefix(c.Name, "ds-md") {
+			return c.Name
+		}
+	}
+	return ""
+}
+
+func getModelProxyContainerName(containers []corev1.Container) string {
+	for _, c := range containers {
+		if strings.HasPrefix(c.Name, "ds-mx") {
+			return c.Name
+		}
+	}
+	return ""
 }
